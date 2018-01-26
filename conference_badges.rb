57a61c98d5ea8f4a8b05722089609f94f910261b
Kim Foukaris <kimfoukaris@outlook.com>
2018-01-26 18:59:49 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,8 @@ end
 def batch_badge_creator(names)
   names.collect{|name| "Hello, my name is #{name}." }
 end
+
+def assign_rooms(names)
+  names.each_with_index{|name, index| names(name) = index}
+  
+end
