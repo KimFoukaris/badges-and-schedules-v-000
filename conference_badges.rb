8969f3ad32448d154857514d78433ad489f20e59
Kim Foukaris <kimfoukaris@outlook.com>
2018-01-26 19:18:24 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,6 +7,6 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(names)
-  names.each_with_index{|name, index| name[room] = index}
+  names.each_with_index{|name, index| puts "Hello, #{name}. You'll be assigned to room #{index+1}"}
 
 end
